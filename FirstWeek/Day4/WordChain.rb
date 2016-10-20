18f4f1611a3b37daedd9e1b85a3b8b62e3100e12
Maxime Mercy <mxmercy@gmail.com>
2016-10-20 22:51:00 +0200

    word chain amelioration division of the method and exit condition if failed

--- FirstWeek/Day4/WordChain.rb
+++ FirstWeek/Day4/WordChain.rb
@@ -10,30 +10,44 @@ class WordChain
 
   def find_chain(start, final)
 
-
-            @start_array = start.each_char.to_a
-           @final_array = final.each_char.to_a
+          puts start
+          @start_array = start.each_char.to_a
+          @final_array = final.each_char.to_a
            
-           test_array = @start_array
+          @test_array = start.each_char.to_a
 
-           i = 0
 
-           while @test_array != @final_array do
-               @saved_array = @test_array
-               @test_array[i] = @final_array[i]
-               i += 1
-               
-               if @dictionary.exists?(@test_array.join) == true
-                   puts @test_array.join
-               elsif @dictionary.exists?(@test_array.join) == false
-                   @test_array = @saved_array
-               end
+          while ( @test_array != @final_array ) && @counter < 200 do
+             array_exploration
+             @counter += 1
            end
 
-   end
+           puts "Game Over: No transformation found" if @counter == 200
+  end
 
+
+
+
+  def array_exploration
+      
+      @test_array.each_with_index do |char,index|
+            if @test_array[index] != @final_array[index]
+                  @test_array[index] = @final_array[index]
+                      if @dictionary.exists?(@test_array.join) != true
+
+                      @test_array[index] = char
+                    else
+                      puts @test_array.join
+                    end
+               
+               end
+            end
+      end
 end
 
 dictionary = Dictionary.from_file("/usr/share/dict/words")
 my_chain = WordChain.new(dictionary)
-my_chain.find_chain("cat", "dog")
+my_chain.find_chain("rodeo", "meteo")
+
+
+

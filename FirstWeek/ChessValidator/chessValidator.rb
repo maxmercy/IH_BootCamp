0817f086cb5230de704640fbbefb232cca90f48b
Maxime Mercy <mxmercy@gmail.com>
2016-10-22 13:32:43 +0200

    ChessValidator: Complete Rock Validation Implement Diagonal Validation for the Queen

--- FirstWeek/ChessValidator/chessValidator.rb
+++ FirstWeek/ChessValidator/chessValidator.rb
@@ -1,9 +1,12 @@
 require "pry"
 require_relative "pieces"
 
+
 class Cheeseboard
+	attr_accessor :board 
 	def initialize
 		@board = []
+		@coordinate = []
 	end
 
 	def create_board
@@ -11,14 +14,32 @@ class Cheeseboard
 		end
 	end
 
+	#### convert position bord in array ie: a1 -> [0,7] !!! coordinatearray up/down!!!
+	def convert_position_to_array(position)
+		@coordinate = position.chars
+		temp = @coordinate[0]
+		x = @coordinate[1]
+		x = (8 - position[1].to_i )
+		convert_y = { a: 0, b: 1, c: 2, d: 3, e: 4, f: 5, g: 6 , h: 7 }
+		y= convert_y.values_at(temp.to_sym)
+		@coordinate[0]=x
+		@coordinate[1]= y[0]
+		position_array = @coordinate
+	end
+
+	#### print board with all piece if there is
 	def print_board
-		puts "**************************************"
-		puts "|                                    |"
-		puts "|          Actual board              |"
-		puts "|                                    |"
-		puts "**************************************"
+		puts "**********************************"
+		puts "|   ♔ ♚ ♕ ♛ ♖ ♜ ♗ ♝ ♘ ♞ ♙ ♟      |"
+		puts "|        Actual board            |"
+		puts "|   ♔ ♚ ♕ ♛ ♖ ♜ ♗ ♝ ♘ ♞ ♙ ♟      |"
+		puts "**********************************"
 		@board.each_index do |i|
 			@board[i].each_index do |y|
+				if y == 0 
+					print 8 - i
+				end
+
 				if @board[i][y] == nil 
 					print "#{@board[i][y]} [_]"
 				else 
@@ -27,51 +48,91 @@ class Cheeseboard
 
 			end
 			puts "\n"
-		end		
+		end	
+		puts "   a   b   c   d   e   f   g   h"
+		puts "\n\n\n\n"
 	end
 
+	#add a piece on the bord
 	def add_piece(piece,position)
-		x = position[0]
-		y = position[1]
+		position_grille = convert_position_to_array(position)
+		x = position_grille[0]
+		y = position_grille[1]
 		@board[x][y] = piece
 	end
 
-	def position_status(position)
-		x = position[0]
-		y = position[1]
 
-		if @board[x][y] != nil
-			puts "At the position #{position}, there is a #{@board[x][y]}"
+	#check the position status
+	def position_status(position)
+
+		if position.is_a? String
+			position_grille = convert_position_to_array(position)
+		else position_grille = position
+		end
+		
+		x = position_grille[0]
+		y = position_grille[1]
+		
+
+		if @board[x][y] == nil
+			#puts "At the position #{position}, there is nothing"
+			false
+			
 		else
-			puts "At the position #{position}, there is nothing"
+			#puts "At the position #{position}, there is a #{@board[x][y]}"
+
+			true
 		end
 	end
+
+
+
+	# def checkmove(piece,origin,destination)
+	# 	origin_array = convert_position_to_array(origin)
+	# 	desctination_array = convert_position_to_array(destination)
+
+	# 	#piece.valid_move(@board, origin_array, desctination_array)
+
+	# end
 
 end
 
 
 
 
+mycheeseboard = Cheeseboard.new
+
+mycheeseboard.create_board
+
+mycheeseboard.add_piece(:wQ, "a1")
+mycheeseboard.add_piece(:wQ, "e1")
+mycheeseboard.add_piece(:wQ, "a5")
+mycheeseboard.add_piece(:bQ, "e5")
+mycheeseboard.add_piece(:bQ, "c8")
+
+mycheeseboard.print_board
+
+# mycheeseboard.position_status("a8")
+# mycheeseboard.position_status("c4")
+
+# mycheeseboard.checkmove(wR,"a1","a4")
+# mycheeseboard.checkmove(wR,"a1","e1")
+# mycheeseboard.checkmove(wR,"a1","e4")
+
+wR = Rock.new
+bB = Queen.new
+wQ = Queen.new
+
+wR.valid_move(mycheeseboard,"a1","a4")
+
+wQ.valid_move(mycheeseboard,"a1","d4")
+wQ.valid_move(mycheeseboard,"d4","a1")
+wQ.valid_move(mycheeseboard,"a1","f1")
+wQ.valid_move(mycheeseboard,"a1","e5")
+wQ.valid_move(mycheeseboard,"a5","d2")
 
 
-# mycheeseboard = Cheeseboard.new
-
-# mycheeseboard.create_board
-# mycheeseboard.print_board
-
-# mycheeseboard.add_piece(:wR, [7,0])
-# mycheeseboard.add_piece(:wR, [7,7])
-# mycheeseboard.add_piece(:bR, [0,0])
-# mycheeseboard.add_piece(:bR, [0,7])
-
-# mycheeseboard.print_board
-
-# mycheeseboard.position_status([0,7])
-# mycheeseboard.position_status([3,7])
-
-rock1 = Rock.new
-
-rock1.respond
 
 
-binding.pry
+
+
--- FirstWeek/ChessValidator/pieces.rb
+++ FirstWeek/ChessValidator/pieces.rb
@@ -1,11 +1,299 @@
 class Rock
+	attr_reader :color
 	def initialize
+		@color
 	end
 
-	def respond
-		puts "toto"
+	#return @color = white or black
+	def piece_color(piece) 
+		list_white_piece = [:wP, :wR, :wN, :wB, :wQ, :wK]
+		if list_white_piece.include?(piece) 
+			@color = "white" 
+		else
+			@color = "black"
+		end
+	end
 
+	#check if color are similar
+	def color_check(piece_at_origin,piece_at_destination)
+		if piece_color(piece_at_origin) == piece_color(piece_at_destination)
+			#puts "NOT VALID, Origin and destination pieces' colors are identical."
+			false
+		else
+			true
+		end
+	end
+
+	#check if movement valid (rock horizontal / vertical )
+	def check_direction(origin_arr,destination_arr)
+		 horizontal_direction(origin_arr,destination_arr) 
 	end
 
 
+
+	def horizontal_direction(origin_arr,destination_arr)	# ## check direction
+		if origin_arr[0] == destination_arr[0] || origin_arr[1] == destination_arr[1]
+			true 
+		else
+			#puts "NO VALID, invalid Direction"
+			false
+		end
+	end
+
+	#check if destination empty
+	def check_if_destination_empty(the_board,destination_arr)
+		if the_board.position_status(destination_arr) == false
+			false
+		else 
+			true
+		end
+	end
+
+
+	#check the case between
+	def path_validator(the_board,origin_arr,destination_arr)
+		validity = true
+		#puts "Starting Position #{origin_arr}, final position #{destination_arr}"
+
+		if origin_arr[0] == destination_arr[0]
+			#puts "Horitzontal direction"	
+			if origin_arr[1] < destination_arr[1]
+				i = origin_arr[1] + 1
+			else 
+				i = origin_arr[1] - 1	
+			end
+			until i == (destination_arr[1])
+				if the_board.board[origin_arr[0]][i] != nil					
+					#puts "Not VALID, A piece at the position [[#{origin_arr[0]}],#{[i]}] was FOUND."		 
+					validity = false
+				end
+				if origin_arr[1] < destination_arr[1]
+					i += 1
+				else 
+					i -= 1
+				end		
+			end
+
+		else 
+			#puts "Vertical direction"	
+			if origin_arr[0] < destination_arr[0]
+
+			 	i = origin_arr[0] + 1
+			 else 
+			 	i = origin_arr[0] - 1	
+			 end
+			 until i == (destination_arr[0])
+			 	if the_board.board[i][origin_arr[1]] != nil					
+			 		#puts "Not VALID, A piece at the position [#{[i]},[#{origin_arr[1]}]] was FOUND."		 					
+					validity = false				
+			 	end
+			 	if origin_arr[0] < destination_arr[0]
+					i += 1
+			 	else 
+			 		i -= 1
+			 	end		
+			end
+
+		end
+		validity
+	end
+
+	def valid_move(the_board,origin_arr,destination_arr)
+		puts "\nYou want to move from #{origin_arr} to #{destination_arr}"
+		if origin_arr.is_a? String
+		origin_arr = the_board.convert_position_to_array(origin_arr)
+		destination_arr = the_board.convert_position_to_array(destination_arr)
+		end
+
+		piece_at_destination = the_board.board[destination_arr[0]][destination_arr[1]]
+		piece_at_origin = the_board.board[origin_arr[0]][origin_arr[1]]
+
+		checker = true 
+		if check_direction(origin_arr,destination_arr) == false
+			checker = false
+			puts "Not valid direction"
+		else
+			
+		end
+		if check_if_destination_empty(the_board,destination_arr) 
+			if color_check(piece_at_origin,piece_at_destination) == false
+				checker = false
+				puts "At destination a piece with the same color."
+			end
+		end
+		if path_validator(the_board,origin_arr,destination_arr) == false
+			checker = false 
+			puts "Path not clear."
+		end
+
+		if checker == true 
+			puts "VALID MOUVEMENT"  
+		else 
+			puts "INVALID MOUVEMENT"
+		end
+	end
 end
+
+
+class Queen
+	attr_reader :color
+	def initialize
+		@color
+	end
+
+	#return @color = white or black
+	def piece_color(piece) 
+		list_white_piece = [:wP, :wR, :wN, :wB, :wQ, :wK]
+		if list_white_piece.include?(piece) 
+			@color = "white" 
+		else
+			@color = "black"
+		end
+	end
+
+	#check if color are similar
+	def color_check(piece_at_origin,piece_at_destination)
+		if piece_color(piece_at_origin) == piece_color(piece_at_destination)
+			#puts "NOT VALID, Origin and destination pieces' colors are identical."
+			false
+		else
+			true
+		end
+	end
+
+
+
+
+
+
+	#check if movement valid (rock horizontal / vertical )
+	def check_direction(origin_arr,destination_arr)
+		 horizontal_direction(origin_arr,destination_arr) || diagonal_direction(origin_arr,destination_arr)
+	end
+
+
+
+	def horizontal_direction(origin_arr,destination_arr)	# ## check direction
+		if origin_arr[0] == destination_arr[0] || origin_arr[1] == destination_arr[1]
+			true 
+		else
+			#puts "NO VALID, invalid Direction"
+			false
+		end
+	end
+
+	def diagonal_direction(origin_arr,destination_arr)
+		dif_x = destination_arr[0]	- origin_arr[0]
+		dif_y = destination_arr[1]	- origin_arr[1]
+		if dif_x == dif_y || dif_x == -dif_y
+			true
+		else
+			false
+		end
+	end
+
+
+
+
+
+
+
+
+
+
+
+
+	#check if destination empty
+	def check_if_destination_empty(the_board,destination_arr)
+		if the_board.position_status(destination_arr) == false
+			false
+		else 
+			true
+		end
+	end
+
+
+	#check the case between
+	def path_validator(the_board,origin_arr,destination_arr)
+		validity = true
+		#puts "Starting Position #{origin_arr}, final position #{destination_arr}"
+
+		if origin_arr[0] == destination_arr[0]
+			#puts "Horitzontal direction"	
+			if origin_arr[1] < destination_arr[1]
+				i = origin_arr[1] + 1
+			else 
+				i = origin_arr[1] - 1	
+			end
+			until i == (destination_arr[1])
+				if the_board.board[origin_arr[0]][i] != nil					
+					#puts "Not VALID, A piece at the position [[#{origin_arr[0]}],#{[i]}] was FOUND."		 
+					validity = false
+				end
+				if origin_arr[1] < destination_arr[1]
+					i += 1
+				else 
+					i -= 1
+				end		
+			end
+
+		else 
+			#puts "Vertical direction"	
+			if origin_arr[0] < destination_arr[0]
+
+			 	i = origin_arr[0] + 1
+			 else 
+			 	i = origin_arr[0] - 1	
+			 end
+			 until i == (destination_arr[0])
+			 	if the_board.board[i][origin_arr[1]] != nil					
+			 		#puts "Not VALID, A piece at the position [#{[i]},[#{origin_arr[1]}]] was FOUND."		 					
+					validity = false				
+			 	end
+			 	if origin_arr[0] < destination_arr[0]
+					i += 1
+			 	else 
+			 		i -= 1
+			 	end		
+			end
+
+		end
+		validity
+	end
+
+	def valid_move(the_board,origin_arr,destination_arr)
+		puts "\nYou want to move from #{origin_arr} to #{destination_arr}"
+		if origin_arr.is_a? String
+		origin_arr = the_board.convert_position_to_array(origin_arr)
+		destination_arr = the_board.convert_position_to_array(destination_arr)
+		end
+
+		piece_at_destination = the_board.board[destination_arr[0]][destination_arr[1]]
+		piece_at_origin = the_board.board[origin_arr[0]][origin_arr[1]]
+
+		checker = true 
+		if check_direction(origin_arr,destination_arr) == false
+			checker = false
+			puts "Not valid direction"
+		else
+			
+		end
+		if check_if_destination_empty(the_board,destination_arr) 
+			if color_check(piece_at_origin,piece_at_destination) == false
+				checker = false
+				puts "At destination a piece with the same color."
+			end
+		end
+		if path_validator(the_board,origin_arr,destination_arr) == false
+			checker = false 
+			puts "Path not clear."
+		end
+
+		if checker == true 
+			puts "VALID MOUVEMENT"  
+		else 
+			puts "INVALID MOUVEMENT"
+		end
+	end
+end
+
